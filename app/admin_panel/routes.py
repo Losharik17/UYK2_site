2f65c50ac3efc38b6c166f733751139c5802efa7
Noylifer <Sobko.2002@mail.ru>
2022-11-22 00:54:23 +0300

    Статьи в админке

--- app/admin_panel/routes.py
+++ app/admin_panel/routes.py
@@ -1,9 +1,10 @@
-from app.admin_panel.views import NewsView, EventView, TextView, PlanView
+from app.admin_panel.views import NewsView, EventView, TextView, PlanView, ArticleView
 from app import admin, db
-from app.models import News, Event, Text, AcademicPlan
+from app.models import News, Event, Text, AcademicPlan, Article
 
 
 admin.add_view(NewsView(News, db.session, name='Новости'))
 admin.add_view(EventView(Event, db.session, name='События'))
 admin.add_view(TextView(Text, db.session, name='Текста'))
 admin.add_view(PlanView(AcademicPlan, db.session, name='Отрезки'))
+admin.add_view(ArticleView(Article, db.session, name='Статьи'))
