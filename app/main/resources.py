d9c0943440d94aaf997aa6af432e89424e672b70
Noylifer <Sobko.2002@mail.ru>
2022-11-21 16:05:48 +0300

    .

--- app/main/resources.py
+++ app/main/resources.py
@@ -4,7 +4,7 @@ from werkzeug.datastructures import FileStorage
 from flask import jsonify, make_response, current_app
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from app.models import News, Event
+from app.models import News, Event, AcademicPlan
 from app import db
 
 
@@ -29,25 +29,29 @@ class TemplateResource(Resource):
 
 class TemplateListResource(Resource):
     MODEL = None  # какая-нибудь таблица из бд
-    SORT_KEY: str = 'id'
+    SORT_KEY: str = None
+    FILTER_KEY: str = None
     PAGE_SIZE: int = 10
-
-    def __init__(self):
-        self.parser = reqparse.RequestParser()
+    ONLY_ACTUAL = False
+    PARSER = reqparse.RequestParser()
 
     def get(self):
         try:
-            self.parser.add_argument('page_number', type=int,
+            self.PARSER.add_argument('page_number', type=int,
                                      default=1, location='args')
             try:
-                args = self.parser.parse_args()
+                args = self.PARSER.parse_args()
                 page_number = args['page_number']
             except:
                 page_number = 1
 
             rows = self.MODEL.query
+            if self.ONLY_ACTUAL:
+                rows = rows.filter(self.MODEL.__dict__[f'{self.FILTER_KEY}']
+                                   >= dt.datetime.now())
             if self.SORT_KEY:
                 rows = rows.order_by(self.MODEL.__dict__[f'{self.SORT_KEY}'])
+
             rows = rows.limit(page_number * self.PAGE_SIZE)
 
             return jsonify(rows[(page_number - 1) * self.PAGE_SIZE:])
@@ -72,4 +76,12 @@ class EventResource(TemplateResource):
 class EventListResource(TemplateListResource):
     MODEL = Event
     SORT_KEY = 'start_date'
+    FILTER_KEY = 'end_date'
     PAGE_SIZE = 8
+    ONLY_ACTUAL = True
+
+
+class PlanListResource(TemplateListResource):
+    MODEL = AcademicPlan
+    SORT_KEY = 'id'
+    PAGE_SIZE = 12
