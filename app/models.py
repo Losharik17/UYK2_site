efec2d1f542888050e3edc00e27f60067ee60946
Норманев Дмитрий <dimanormanev@yandex.ru>
2022-11-21 18:11:26 +0300

    БД правильное оформление

--- app/models.py
+++ app/models.py
@@ -5,10 +5,13 @@ from app import db
 from flask import url_for
 
 
+@dataclass
 class ImageFunctions:
     ADDRESS_URL = 'http://185.87.50.137:8080'
     PATH = None  # путь из папки static
     id = None  # id записи
+    img_url: str  # относительный url
+    full_img_url: str  # полный url
 
     def save_img(self, img):
         """Сохраняет изображение"""
@@ -26,7 +29,7 @@ class ImageFunctions:
         self.save_img(img)
 
     @property
-    def img_url(self):
+    def img_url(self) -> str:
         """Возвращает url для изображения"""
         relative_path = os.path.join(self.PATH, f'{self.id}.jpg')
         url_path = url_for('static', filename=relative_path)
@@ -36,7 +39,7 @@ class ImageFunctions:
         return url_for('static', filename=f'{self.PATH}0.jpg')
 
     @property
-    def full_img_url(self):
+    def full_img_url(self) -> str:
         return f'{self.ADDRESS_URL}{self.img_url}'
 
 
@@ -44,67 +47,38 @@ class ImageFunctions:
 class News(db.Model, ImageFunctions):
     PATH = 'images/news/'
 
-    id = db.Column(db.Integer, primary_key=True, index=True)
-    title = db.Column(db.String(256))
-    text = db.Column(db.Text)
-    date = db.Column(db.Date)
-    link = db.Column(db.String(4096))
-    author = db.Column(db.String(128))
-
-    id: int
-    title: str
-    text: str
-    date: field(default_factory=dt.datetime.now)
-    link: str
-    author: str
-    img_url: str
-    full_img_url: str
+    id: int = db.Column(db.Integer, primary_key=True, index=True)
+    title: str = db.Column(db.String(256))
+    text: str = db.Column(db.Text)
+    date: str = db.Column(db.Date)
+    link: str = db.Column(db.String(4096))
+    author: str = db.Column(db.String(128))
 
 
 @dataclass
 class Event(db.Model, ImageFunctions):
     PATH = 'images/events/'
 
-    id = db.Column(db.Integer, primary_key=True, index=True)
-    title = db.Column(db.String(256))
-    text = db.Column(db.Text)
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
-    address = db.Column(db.String(256))
-    link = db.Column(db.String(4096))
-
-    id: int
-    title: str
-    text: str
-    start_date: field(default=None)
-    end_date: field(default=None)
-    address: str
-    link: str
-    img_url: str
-    full_img_url: str
+    id: int = db.Column(db.Integer, primary_key=True, index=True)
+    title: str = db.Column(db.String(256))
+    text: str = db.Column(db.Text)
+    start_date: str = db.Column(db.Date)
+    end_date: str = db.Column(db.Date)
+    address: str = db.Column(db.String(256))
+    link: str = db.Column(db.String(4096))
 
 
 @dataclass
 class Text(db.Model):
-    id = db.Column(db.Integer, primary_key=True, index=True)
-    title = db.Column(db.String(256))
-    text = db.Column(db.Text)
-
-    id: int
-    title: str
-    text: str
+    id: int = db.Column(db.Integer, primary_key=True, index=True)
+    title: str = db.Column(db.String(256))
+    text: str = db.Column(db.Text)
 
 
 @dataclass
 class AcademicPlan(db.Model, ImageFunctions):
     PATH = 'images/academic_plans/'
 
-    id = db.Column(db.Integer, primary_key=True, index=True)
-    course = db.Column(db.Integer)
-    semester = db.Column(db.Integer)
-
-    id: int
-    course: int
-    semester: int
-    img_url: str
-    full_img_url: str
+    id: int = db.Column(db.Integer, primary_key=True, index=True)
+    course: int = db.Column(db.Integer)
+    semester: int = db.Column(db.Integer)
